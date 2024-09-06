df061c2d3f2ccb4b107d6028bc2ae1fe29e717c9
Matheus Adorno <matheusadorno11@gmail.com>
2024-09-06 11:41:38 -0300

    adjusting screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ThankYouPage = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+    <div className="flex flex-col justify-center items-center bg-gray-100 mt-40">
       <div className="bg-white shadow-lg rounded-lg p-8 max-w-lg text-center">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">Obrigado por se inscrever!</h1>
         <p className="text-gray-600">
